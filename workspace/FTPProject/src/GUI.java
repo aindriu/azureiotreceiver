f39b7880376927892eee8f4cd47562be728cb162
aindriu <cartonandrew@gmail.com>
2017-05-17 12:26:07 +0100

    n

---
+++ workspace/FTPProject/src/GUI.java
@@ -0,0 +1,184 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class GUI {
+
+	private JLabel imageLabel;
+	private LocalFTPClient localFTPClient;
+	
+	public GUI(LocalFTPClient f) {
+		this.localFTPClient = f;
+	}
+
+	public static void main(String[] args) {
+		LocalFTPClient f = new LocalFTPClient();
+		
+		GUI t = new GUI(f);
+		t.init();
+
+		
+		//f.createNewestFolder("192.168.1.201");
+		
+	}
+
+	private void init() {
+		Properties prop = new Properties();
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+		String hostname = "127.0.0.1";
+		String port = "21";
+		String username = "andrew";
+		String password = "test";
+		
+		if (inputStream != null)
+		{
+			try {
+				prop.load(inputStream);
+				hostname = prop.getProperty("hostname");
+				port = prop.getProperty("port");
+				username = prop.getProperty("username");
+				password = prop.getProperty("password");
+				
+			} catch (IOException e1) {
+				hostname = "127.0.0.1";
+				port = "21";
+				username = "andrew";
+				password = "test";
+			}
+		}
+		
+		localFTPClient.setHostname(hostname);
+		localFTPClient.setPort(port);
+		localFTPClient.setUsername(username);
+		localFTPClient.setPassword(password);
+		
+		GridBagLayout gbl = new GridBagLayout();
+		JFrame frame = new JFrame("Images");
+		frame.setLayout(gbl);
+
+		frame.setVisible(true);
+
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.gridx = 1;
+		gc.gridy = 1;
+		gc.insets = new Insets(5, 5, 5, 5);
+
+		JLabel conn = new JLabel("Host Address:");
+		frame.add(conn, gc);
+
+		gc = new GridBagConstraints();
+		gc.gridx = 2;
+		gc.gridy = 1;
+		gc.insets = new Insets(5, 5, 5, 5);
+
+		JTextField hostString = new JTextField(
+				hostname, 20);
+
+		frame.add(hostString, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 3;
+		gc.gridy = 1;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JLabel portL = new JLabel("Port:");
+		frame.add(portL, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 4;
+		gc.gridy = 1;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JTextField portT = new JTextField(port,7);
+		frame.add(portT, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 1;
+		gc.gridy = 2;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JLabel usernameL = new JLabel("Username:");
+		frame.add(usernameL, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 2;
+		gc.gridy = 2;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JTextField usernameT = new JTextField(username, 20);
+		frame.add(usernameT, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 3;
+		gc.gridy = 2;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JLabel passL = new JLabel("Password:");
+		frame.add(passL, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 4;
+		gc.gridy = 2;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JPasswordField passT = new JPasswordField(password,15);
+		frame.add(passT, gc);
+		
+		gc = new GridBagConstraints();
+		gc.gridx = 5;
+		gc.gridy = 3;
+		gc.insets = new Insets(5, 5, 5, 5);
+		JButton connectB = new JButton("Connect");
+		frame.add(connectB, gc);
+		
+		/*
+		connectB.addActionListener(new ActionListener() {
+
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				LocalFTPClient f = new LocalFTPClient();
+				File fl = f.downloadNewestFile();
+				if (fl != null)
+				{
+					ImageIcon image = new ImageIcon(fl.getAbsolutePath());
+					image.getImage().flush();
+					imageLabel.setIcon(image);
+					frame.pack();
+					frame.repaint();
+				}
+				
+			} });
+		
+		*/
+		gc = new GridBagConstraints();
+		gc.gridx = 1;
+		gc.gridy = 4;
+		gc.insets = new Insets(5, 5, 5, 5);
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.gridheight = GridBagConstraints.REMAINDER;
+		
+		ImageIcon image = new ImageIcon("");
+        imageLabel = new JLabel(image); 
+        frame.add(imageLabel,gc);
+        
+        frame.pack();
+		frame.setVisible(true);
+        	
+	}
+}
---
+++ workspace/FTPProject/src/LocalFTPClient.java
@@ -0,0 +1,216 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.LinkedList;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class LocalFTPClient {
+
+	private String hostname;
+	private String port;
+	private String username;
+	private String password;
+	
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static void main(String[] args) {
+		
+		LocalFTPClient f = new LocalFTPClient();
+		f.createNewestFolder("192.168.1.201");
+	}
+	
+	public void createNewestFolder(String ipAddress)
+	{
+		Date date = new Date();
+		String folderName;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy_MM");
+		String dateS = format.format(date);
+		
+		
+		folderName = "" + dateS + "_" + ipAddress;
+		
+		FTPClient ftp = new FTPClient();
+	    FTPClientConfig config = new FTPClientConfig();
+	    ArrayList<FTPFile> filesArray = new ArrayList<FTPFile>(); 
+	    
+	    ftp.configure(config);
+	    boolean error = false;
+	    try {
+	      int reply;
+	      String server = "localhost";
+	      ftp.connect(server);
+	      
+	      reply = ftp.getReplyCode();
+
+	      if(!FTPReply.isPositiveCompletion(reply)) {
+	        ftp.disconnect();
+	        System.err.println("FTP server refused connection.");
+	        System.exit(1);
+	      }
+	      ftp.login("andrew", "test");
+	      String dir = ftp.printWorkingDirectory();
+	      
+	      FTPFile[] files = ftp.listFiles(dir);
+	      
+	      
+	      for (int i = 0; i < files.length; i++)
+	      {
+	    	  if (files[i].getName().equals(folderName))
+	    		  return;
+	    	  
+	      }
+
+	      boolean s = ftp.makeDirectory(dir + folderName);
+	      
+	      ftp.logout();
+	    } catch(IOException e) {
+	      error = true;
+	      e.printStackTrace();
+	    } finally {
+	      if(ftp.isConnected()) {
+	        try {
+	          ftp.disconnect();
+	        } catch(IOException ioe) {
+	          // do nothing
+	        }
+	      }
+	    }
+	}
+	public File downloadNewestFile() {
+		FTPClient ftp = new FTPClient();
+	    FTPClientConfig config = new FTPClientConfig();
+	    ArrayList<FTPFile> filesArray = new ArrayList<FTPFile>(); 
+	    
+	    ftp.configure(config);
+	    boolean error = false;
+	    try {
+	      int reply;
+	      String server = "localhost";
+	      ftp.connect(server);
+	      
+
+	      
+	      reply = ftp.getReplyCode();
+
+	      if(!FTPReply.isPositiveCompletion(reply)) {
+	        ftp.disconnect();
+	        System.err.println("FTP server refused connection.");
+	        System.exit(1);
+	      }
+	      ftp.login("andrew", "test");
+	      String dir = ftp.printWorkingDirectory();
+	      //System.out.println(dir);
+	      FTPFile[] files = ftp.listFiles(dir);
+	      
+	      for (int i = 0; i < files.length; i++)
+	      {
+	    	  Calendar c = files[i].getTimestamp();
+	    	  //System.out.println(files[i] + " " + c.getTimeInMillis());
+	    	  filesArray.add(files[i]);
+	      }
+	      
+	      
+	      LinkedList<FTPFile> filesToDelete = new LinkedList<FTPFile>();
+	      for (FTPFile f : filesArray)
+	      {
+	    	  if (f.isDirectory())
+	    		  filesToDelete.add(f);
+	      }
+	      
+	      Collections.sort(filesArray, new Comparator<FTPFile>() {
+
+			@Override
+			public int compare(FTPFile arg0, FTPFile arg1) {
+				return arg1.getTimestamp().compareTo(arg0.getTimestamp());
+			} } );
+	      
+	      
+	      for (FTPFile f : filesToDelete)
+	    	  filesArray.remove(f);
+	      
+	      
+	     
+	      
+	      if (filesArray.size() > 0)
+	      {
+	    	  FTPFile lastEntry = filesArray.get(0);
+	    	  FTPFile file = lastEntry;
+	    	  ftp.enterLocalPassiveMode();
+	          ftp.setFileType(ftp.BINARY_FILE_TYPE);
+	          
+	          String remoteFile1 = "/" + file.getName();
+	          File downloadFile1 = new File(System.getProperty("user.dir") + "\\" + file.getTimestamp().getTimeInMillis() + "-" + file.getName() );
+	          OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+	            boolean success = ftp.retrieveFile(remoteFile1, outputStream1);
+	            outputStream1.close();
+	 
+	            if (success) {
+	                //System.out.println("File #1 has been downloaded successfully.");
+	                ftp.logout();
+	                return downloadFile1;
+	            }
+	            
+	            return null;
+	            
+	      }
+	      
+	      
+	      ftp.logout();
+	    } catch(IOException e) {
+	      error = true;
+	      e.printStackTrace();
+	    } finally {
+	      if(ftp.isConnected()) {
+	        try {
+	          ftp.disconnect();
+	        } catch(IOException ioe) {
+	          // do nothing
+	        }
+	      }
+	    }
+	    return null;
+	}
+
+}
